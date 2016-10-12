f14028d6c396b26498bedb8a827c87bcaad4670b
Ivy Chang <ivycwq@icloud.com>
2016-10-12 15:09:15 -0700

    adding properties

--- Book.cs
+++ Book.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System.Collections.Generic;
+
+namespace Library {
+  public class Book {
+    public int YearOfPublication {get; set;}
+    public string Title {get; set;}
+    public string LCNumber {get; set;}
+    public string Publisher {get; set;}
+    public List<string> authors;
+    public List<string> subjectHeadings;
+    public bool Circulating {get; set;}
+
+    
+  }
+}
